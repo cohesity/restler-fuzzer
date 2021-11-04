62a56870235b439ef3fb919e2450b02783ac56ed
Ivan Mudarth <ivan.mudarth@yahoo.com>
2021-11-04 11:42:08 -0400

    add test summary

--- restler-dashboard/scripts/main.js
+++ restler-dashboard/scripts/main.js
@@ -2,5 +2,10 @@ var json2plain = require('json2plain');
 
 var json = require('../../demo/Test/RestlerResults/experiment77870/logs/testing_summary.json');
 var plain = json2plain(json);
+while (plain[0] == "\n") 
+    plain = plain.substring(1 , plain.length - 1);
 
-console.log(plain);
+var fs = require('fs');
+fs.writeFile('../data/test_summary.txt', plain, function(err, result) {
+    if(err) console.log('error', err);
+});
