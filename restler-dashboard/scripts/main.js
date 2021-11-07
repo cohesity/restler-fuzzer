7686de4df44768bb5a4c1d101778a4fa45efbc6d
Ivan Mudarth <ivan.mudarth@yahoo.com>
2021-11-07 17:11:53 -0500

    add compile page

--- restler-dashboard/scripts/main.js
+++ restler-dashboard/scripts/main.js
@@ -1,5 +1,5 @@
 // Format test summary
-function format_test_summary(path, filename) {
+function json_to_txt(path, filename) {
     var json2plain = require('json2plain');
 
     var json = require('../../demo/' + path);
@@ -13,16 +13,38 @@ function format_test_summary(path, filename) {
     });
 }
 
+// Compile Page
+var dict_path = "Compile/dict.json"; 
+var dict_filename = "compile_dict.txt"
+json_to_txt(dict_path, dict_filename);
+
+// Compile Page
+var config_path = "Compile/config.json"; 
+var config_filename = "compile_config.txt"
+json_to_txt(config_path, config_filename);
+
+// Compile Page
+var settings_path = "Compile/engine_settings.json"; 
+var settings_filename = "compile_settings.txt"
+json_to_txt(settings_path, settings_filename);
+
+// Test Page
 var test_path = "Test/RestlerResults/experiment77870/logs/testing_summary.json";
 var test_filename = "test_summary.txt"
-format_test_summary(test_path, test_filename);
+json_to_txt(test_path, test_filename);
 
+// Fuzz-lean page
 var fuzzlean_path = "FuzzLean/RestlerResults/experiment77894/logs/testing_summary.json"; 
 var fuzzlean_filename = "fuzzlean_summary.txt"
-format_test_summary(fuzzlean_path, fuzzlean_filename);
+json_to_txt(fuzzlean_path, fuzzlean_filename);
 
+// Fuzz page
 var fuzz_path = "Fuzz/RestlerResults/experiment81109/logs/testing_summary.json"; 
 var fuzz_filename = "fuzz_summary.txt"
-format_test_summary(fuzz_path, fuzz_filename);
+json_to_txt(fuzz_path, fuzz_filename);
+
+// let filePath = "../../demo/Test/RestlerResults/experiment77870/logs/speccov.json";
+// let test_data = require(filePath)
+// console.log(test_data)
 
 // Format test speccov data
