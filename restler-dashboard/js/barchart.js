50180c59a0fa860c030478b7f016935f3f767f54
Ivan Mudarth <ivan.mudarth@yahoo.com>
2021-11-09 16:04:35 -0500

    add bar graph

---
+++ restler-dashboard/js/barchart.js
@@ -0,0 +1,72 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#292b2c';
+
+// Bar Chart Example
+var ctx = document.getElementById("myBarChart");
+var myLineChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["3bf5ce32", "422c6758", "d66d829c", "7e243418", "69c3479a", "53bd031a",
+    "b43a9a49", "d58fe78a", "9d6aa1ac"],
+    datasets: [{
+      label: "400",
+      backgroundColor: "#198754",
+      borderColor: "rgba(2,117,216,1)",
+      data: [14, 12, 5, 8, 12, 10, 5, 2, 10],
+    
+    //   options: {
+    //     parsing: {
+    //         key: 'nested.value'
+    //     }
+    // }
+    },
+    // {
+    //   label: "404",
+    //   backgroundColor: "#0d6efd",
+    //   borderColor: "rgba(2,117,216,1)",
+    //   data: [14, 12, 5, 8, 12, 10, 5, 2, 10],
+    // },
+    // {
+    //   label: "500",
+    //   backgroundColor: "#dc3545",
+    //   borderColor: "rgba(2,117,216,1)",
+    //   data: [14, 12, 5, 8, 12, 10, 5, 2, 10],
+    // }
+  ],
+  },
+    options: {
+      parsing: {
+        xAxisKey: 'id',
+        yAxisKey: 'nested.value'
+    },
+    scales: {
+      xAxes: [{
+        gridLines: {
+          display: true
+        },
+        scaleLabel: {
+          display: true,
+          labelString: 'Sequence ID'
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 16,
+        },
+        gridLines: {
+          display: true
+        },
+        scaleLabel: {
+          display: true,
+          labelString: 'Sequence Length'
+        }
+      }],
+    },
+    legend: {
+      display: true,
+      position: "right"
+    }
+  }
+});
--- restler-dashboard/js/datatables.js
+++ restler-dashboard/js/datatables.js
@@ -1,19 +1,3 @@
-function loadJSON(filePath) { 
-    console.log("here")
-	var xhr = new XMLHttpRequest(); 
-	xhr.onreadystatechange = function() { 
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status === 200) { 
-                    if (success) 
-                        console.log(JSON.parse(xhr.responseText)); 
-            } else { 
-                console.log("error")
-            } 
-        }; 
-        xhr.open("GET", filePath, true); 
-        xhr.send(); 
-    }
-} 
 
 window.addEventListener('DOMContentLoaded', event => {
     // Simple-DataTables
@@ -31,92 +15,127 @@ window.addEventListener('DOMContentLoaded', event => {
     // fetch(filePath) 
 	// .then(response => response.json()) 
 	// .then(parsed => console.log(parsed/* parsed contains the parsed json object */)); 
-    loadJSON(filePath);
 
     // var test_data = JSON.parse(fs.readFileSync("../../demo/Test/RestlerResults/experiment77870/logs/speccov.json"));
     // import test_data from "../../demo/Test/RestlerResults/experiment77870/logs/speccov.json";
 
-    let test_data = [
-        {
-            "prop1": "value1",
-            "prop2": "value2",
-            "prop3": "value3"
-        },
-        {
-            "prop1": "value4",
-            "prop2": "value5",
-            "prop3": "value6"
-        }
-    ];
+    // let test_data = [
+    //     {
+    //         "prop1": "value1",
+    //         "prop2": "value2",
+    //         "prop3": "value3"
+    //     },
+    //     {
+    //         "prop1": "value4",
+    //         "prop2": "value5",
+    //         "prop3": "value6"
+    //     }
+    // ];
     
-    let obj = {
-        // Quickly get the headings
-        headings: Object.keys(test_data[0]),
-        // data array
-        data: []
-    };
+    // let obj = {
+    //     // Quickly get the headings
+    //     headings: Object.keys(test_data[0]),
+    //     // data array
+    //     data: []
+    // };
     
-    // Loop over the objects to get the values
-    for ( let i = 0; i < test_data.length; i++ ) {
-        obj.data[i] = [];
-        for (let p in test_data[i]) {
-            if( test_data[i].hasOwnProperty(p) ) {
-                obj.data[i].push(test_data[i][p]);
-            }
-        }
-    }
-
-    console.log(obj)
+    // // Loop over the objects to get the values
+    // for ( let i = 0; i < test_data.length; i++ ) {
+    //     obj.data[i] = [];
+    //     for (let p in test_data[i]) {
+    //         if( test_data[i].hasOwnProperty(p) ) {
+    //             obj.data[i].push(test_data[i][p]);
+    //         }
+    //     }
+    // }
+    
+    let test_data = {
+        "headings": [
+            "HTTP Method",
+            "Endpoint",
+            "Status Code",
+            "Request URI",
+        ],
+        "data": [
+            [
+                "GET",
+                "/blog/posts",
+                "200",
+                "/api/blog/posts?per_page=2&page=1",
+            ],
+            [
+                "POST",
+                "/blog/posts",
+                "201",
+                "/api/blog/posts",
+            ], 
+            [
+                "GET",
+                "/doc",
+                "200",
+                "/api/doc"
+            ],
+            [
+                "DELETE",
+                "/blog/posts/{postId}",
+                "200",
+                "/api/blog/posts/5874"
+            ],
+            [
+                "PUT",
+                "/blog/posts/{postId}",
+                "200",
+                "/api/blog/posts/5875"
+            ],
+            [
+                "GET",
+                "/blog/posts/{postId}",
+                "200",
+                "/api/blog/posts/5880"
+            ],
+            [
+                "GET",
+                "/blog/posts",
+                "200",
+                "/api/blog/posts?per_page=2&page=1",
+            ],
+            [
+                "POST",
+                "/blog/posts",
+                "201",
+                "/api/blog/posts",
+            ], 
+            [
+                "GET",
+                "/doc",
+                "200",
+                "/api/doc"
+            ],
+            [
+                "DELETE",
+                "/blog/posts/{postId}",
+                "200",
+                "/api/blog/posts/5874"
+            ],
+            [
+                "PUT",
+                "/blog/posts/{postId}",
+                "200",
+                "/api/blog/posts/5875"
+            ],
+            [
+                "GET",
+                "/blog/posts/{postId}",
+                "200",
+                "/api/blog/posts/5880"
+            ]
+        ]};
+    
+    // console.log(obj)
     if (datatablesSimple) {
         new simpleDatatables.DataTable(datatablesSimple, {
-            data: obj
+            data: test_data
         });
     }
 });
 
-
-let test_data = {
-    "headings": [
-        "HTTP Method",
-        "Endpoint",
-        "Status Code",
-        "Request URI",
-    ],
-    "data": [
-        [
-            "GET",
-            "/blog/posts",
-            "200",
-            "/api/blog/posts?per_page=2&page=1",
-        ],
-        [
-            "POST",
-            "/blog/posts",
-            "201",
-            "/api/blog/posts",
-        ], 
-        [
-            "GET",
-            "/doc",
-            "200",
-            "/api/doc"
-        ],
-        [
-            "DELETE",
-            "/blog/posts/{postId}",
-            "200",
-            "/api/blog/posts/5874"
-        ],
-        [
-            "PUT",
-            "/blog/posts/{postId}",
-            "200",
-            "/api/blog/posts/5875"
-        ],
-        [
-            "GET",
-            "/blog/posts/{postId}",
-            "200",
-            "/api/blog/posts/5880"
-        ]
-    ]};
